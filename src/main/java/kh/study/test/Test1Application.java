8998a3f7c6ae8428a4f53085048a3b33f8b09071
miinha <alsgk27@gmail.com>
2022-10-27 11:34:33 +0900

    sorry...

--- src/main/java/kh/study/test/Test1Application.java
+++ src/main/java/kh/study/test/Test1Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Test1Application {
-
 	public static void main(String[] args) {
-		
 		//집에서 작업한 주석~
 		SpringApplication.run(Test1Application.class, args);
 		//2일차 강의실에서 작업
